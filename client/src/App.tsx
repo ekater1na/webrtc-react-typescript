7fd596c6ddf60355cf62610cc0613c2c626bb17a
ekater1na <ekaterina_l@tut.by>
2022-12-17 19:55:39 +0300

    feat: add eslint and prettier

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import socketIO from 'socket.io-client';
@@ -6,9 +6,9 @@ import socketIO from 'socket.io-client';
 const WS = 'http://localhost:8080';
 
 function App() {
-  useEffect(()=> {
-    socketIO(WS)
-  }, [])
+  useEffect(() => {
+    socketIO(WS);
+  }, []);
 
   return (
     <div className="App">
