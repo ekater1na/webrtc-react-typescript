6d1d0c3fd79a9082dfad5c8312b09c02866d8178
ekater1na <ekaterina_l@tut.by>
2022-12-17 16:58:28 +0300

    feat: add prettier

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,16 @@
-import express from 'express';
+import express from "express";
 import http from "http";
+import { Server } from "socket.io";
 
 const port = 8000;
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server);
 
+io.on("connection", () => {
+  console.log("User is connected");
+});
 
 server.listen(port, () => {
-    console.log(`Listening to the server on port ${port}`)
-})
+  console.log(`Listening to the server on port ${port}`);
+});
