c54ddda8daa60cbbced9a3b018a9fe6fc40a6018
ekater1na <ekaterina_l@tut.by>
2022-12-17 19:12:46 +0300

    feat: add ws connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,25 @@
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
+import cors from "cors";
 
-const port = 8000;
+const port = 8080;
 const app = express();
+app.use(cors);
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
 
-io.on("connection", () => {
+io.on("connection", (socket) => {
   console.log("User is connected");
+
+  socket.on("disconnect", () => {
+    console.log("User is disconnected")
+  })
 });
 
 server.listen(port, () => {
